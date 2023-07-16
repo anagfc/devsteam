7aa4c00b517e77e5881155bc9479ebf3a844bdaf
anagfc <116309863+anagfc@users.noreply.github.com>
2023-07-16 15:17:22 -0300

    Criado salecard

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
+import SaleCard from '@/components/cards/saleCard/saleCard'
 
 export default function Home() {
   return (
@@ -17,7 +18,10 @@ export default function Home() {
         <Navbar />
         <Container>
           <div>
-          <Subtitle>Promoções</Subtitle>
+            <Subtitle>Promoções</Subtitle>
+            <div>
+              <SaleCard/>
+            </div>
           </div>
           <div>
             <Subtitle>Outros jogos</Subtitle>
